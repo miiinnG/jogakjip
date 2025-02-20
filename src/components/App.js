019a7aa8c916d709adba6032b506c09fb2b10286
JANGHYUNK <janghyun0401@naver.com>
2025-02-20 17:50:03 +0900

    Info 라우팅

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,8 @@ import NotFoundPage from "../pages/NotFoundPage";
 import MemoryCreatePage from "../pages/MemoryCreatePage";
 import PrivateMemoryAccessPage from "../pages/PrivateMemoryAccessPage";
 import MemoryPage from "../pages/MemoryPage";
+import GroupInfo from "./GroupInfo";
+import PostDetail from "./PostDetail";
 
 function App() {
   return (
@@ -19,16 +21,23 @@ function App() {
         <Route path="/" element={<PublicGroupListPage />} />
         <Route path="/private" element={<PrivateGroupListPage />} />
         <Route path="/create-group" element={<CreateGroupPage />} />
-        <Route path="/private-group-access" element={<PrivateGroupAccessPage />} />
+        <Route
+          path="/private-group-access"
+          element={<PrivateGroupAccessPage />}
+        />
         <Route path="/empty-public" element={<EmptyPublicGroupListPage />} />
 
         <Route path="/groups/posts/create" element={<MemoryCreatePage />} />
         {/*"/groups/:groupId/posts/create"*/}
-        <Route path="/groups/posts/access" element={<PrivateMemoryAccessPage />} />
+        <Route
+          path="/groups/posts/access"
+          element={<PrivateMemoryAccessPage />}
+        />
         {/*"/groups/:groupId/posts/:postId/access"*/}
         <Route path="/groups/posts" element={<MemoryPage />} />
         {/*"/groups/:groupId/posts/:postId"*/}
-        
+        <Route path="/groups/info" element={<GroupInfo />} />
+        <Route path="/groups/posts/:id" element={<PostDetail />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Layout>
